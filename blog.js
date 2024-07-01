8abddf7ef9ae7c797fa24c9faa78b789e0f1e703
Deepes2023 <officialdeepeshsg@gmail.com>
2024-07-01 10:05:08 +0530

    implemented database functionality and post request

---
+++ blog.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const url = process.env.MONGODB_URL;
+
+mongoose.connect(url);
+
+const blogSchema = new mongoose.Schema({
+  content: String,
+});
+
+const Blog = mongoose.model("blog", blogSchema);
+
+module.exports = Blog;
